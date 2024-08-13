33046896256122a208299300641b0c2b01a8d321
Kegen Guyll <guyllkegen@gmail.com>
2024-08-13 14:30:45 -0500

    updated login page components to nextui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "../../firebase";
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -37,53 +40,30 @@ export default function Login() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-8">
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Speak thy secret word!
-          </h1>
+    <main className="flex flex-col items-center justify-center p-8">
+      <Card className="w-full max-w-[300px] md:max-w-[320px] lg:max-w-96">
+        <CardHeader>
+          <h4>Login</h4>
+        </CardHeader>
+        <CardBody>
           <form
             onSubmit={handleSubmit}
             className="space-y-4 md:space-y-6"
             action="#"
           >
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Your email
-              </label>
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@company.com"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                id="password"
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-              />
-            </div>
+            <Input
+              label="Email"
+              placeholder="name@company.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              label="Password"
+              placeholder="••••••••"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             {error && (
               <div
                 className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
@@ -92,24 +72,21 @@ export default function Login() {
                 <span className="block sm:inline">{error}</span>
               </div>
             )}
-            <button
-              type="submit"
-              className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
-            >
-              Enter
-            </button>
+            <Button className="w-full" type="submit" >
+              Login
+            </Button>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
               Don&apos;t have an account?{" "}
               <Link
                 href="/register"
-                className="font-medium text-gray-600 hover:underline dark:text-gray-500"
+                className="font-medium text-blue-500 hover:underline"
               >
                 Register here
               </Link>
             </p>
           </form>
-        </div>
-      </div>
+        </CardBody>
+      </Card>
     </main>
   );
 }
