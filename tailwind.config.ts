6ca98f71b69463574b44def9a016a54159352245
Kegen Guyll <guyllkegen@gmail.com>
2024-08-15 15:31:28 -0500

    created custom aaspectRatio for playingCards

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      aspectRatio: {
+        'playingCard': '63 / 88',
+      }
     },
   },
   darkMode: "media",
